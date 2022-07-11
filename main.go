3e5ae80b068f31d84d1763118449d4126950bf7d
Daniel <d94.zaragoza@gmail.com>
2022-07-11 15:46:13 +0200

    feat(models): add model for records

--- main.go
+++ main.go
@@ -1,7 +1,52 @@
 package main
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+	"github.com/urfave/cli/v2"
+	"log"
+	"meeble/models"
+	"os"
+)
 
 func main() {
-	fmt.Println("Hello, world!")
+	var mood string
+	var reason string
+
+	app := &cli.App{
+		Name:                 "meeble",
+		Usage:                "keep track of your mood",
+		Version:              "v0.0.1",
+		EnableBashCompletion: true,
+		Flags: []cli.Flag{
+			&cli.StringFlag{
+				Name:        "mood",
+				Aliases:     []string{"m"},
+				Value:       "",
+				Usage:       "your current mood",
+				Destination: &mood,
+			},
+			&cli.StringFlag{
+				Name:        "reason",
+				Aliases:     []string{"r"},
+				Value:       "",
+				Usage:       "the reason of your mood",
+				Destination: &reason,
+			},
+		},
+		Action: func(ctx *cli.Context) error {
+			if mood == "" {
+				return errors.New("you have to specify what is your mood")
+			}
+
+			record := models.NewRecord(mood, reason)
+			fmt.Printf("record created successfully: %s: %s", record.Mood, record.Reason)
+
+			return nil
+		},
+	}
+
+	if err := app.Run(os.Args); err != nil {
+		log.Fatalln(err)
+	}
 }
